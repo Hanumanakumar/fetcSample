d36f7d77f8d4f6ef31f455a6668eac264ca11740
hanuman <hanumankumar035@gmail.com>
2018-06-20 17:39:38 +0530

    uploaded successfully

---
+++ data.js
@@ -0,0 +1,47 @@
+
+function loadJSON(url) {
+  return new Promise((resolve, reject) => {
+    return fetch(url).then(response => {
+      if (response.ok) {
+        resolve(response.json());
+      } else {
+        reject(new Error('error'));
+      }
+    })
+  })
+}
+
+var myPromise = loadJSON("data.json");
+myPromise.then(data => {
+  // Here's a list of repos!
+  console.log(data);
+  products(data.products);
+});
+
+var mainDiv=document.getElementById("flexParent");
+
+function products(proInfo){
+for(var i=0; i<proInfo.length; i++){
+	var childDiv=document.createElement("div");
+	childDiv.classList.add("flexChild");
+	var pro_name=document.createElement("h2");
+	pro_name.textContent=proInfo[i].name;
+	childDiv.appendChild(pro_name);
+
+	var pro_img=document.createElement("img");
+	pro_img.src=proInfo[i].image;
+
+	childDiv.appendChild(pro_img);
+	mainDiv.appendChild(childDiv);
+
+	var pro_price=document.createElement("h3");
+	pro_price.textContent=proInfo[i].price;
+
+	childDiv.appendChild(pro_price);
+
+	var button=document.createElement("button");
+	childDiv.appendChild(button);
+
+	button.textContent="Add to Cart";
+}
+}
